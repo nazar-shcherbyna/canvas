42402a42ac2b256df54367eb90e99e121207ec78
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2022-07-17 16:53:57 +0300

    refactor(TopPanel)

--- src/components/TopPanel/styles.ts
+++ src/components/TopPanel/styles.ts
@@ -5,20 +5,16 @@ export const TopPanelSC = styled.div`
     align-items: center;
     justify-content: right;
     padding: 10px 20px 10px;
+`
 
-    & > .canvas-button {
-        width: 50px;
-        height: 20px;
-        border: 2px solid blue;
-        border-radius: 5px;
-        cursor: pointer;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        & > svg {
-            max-width: 100%;
-            max-height: 100%;
-        }
-    }
+export const TopPanelBtnSC = styled.div`
+    min-width: 50px;
+    height: 20px;
+    border: 2px solid blue;
+    border-radius: 5px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: 20px;
 `
--- src/components/WorkPlace/index.tsx
+++ src/components/WorkPlace/index.tsx
@@ -1,19 +1,13 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import { Canvas } from '../Canvas'
 import { TopPanel } from '../TopPanel'
 import { WorkPlaceSC } from './styles'
 
 export const WorkPlace = () => {
 
-    const topPanelRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-      console.log('topPanelRef',topPanelRef.current);
-    }, [])
-    
     return (
         <WorkPlaceSC>
-            <TopPanel ref={topPanelRef}/>
+            <TopPanel />
             <Canvas />
         </WorkPlaceSC>
     )
